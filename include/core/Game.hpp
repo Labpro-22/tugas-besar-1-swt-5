d09355e2cf33f844d646e7f6231983ec8bc0989a
NikSamSim <nikosimanjuntak71@gmail.com>
2026-04-21 14:40:42 +0700

    add Game and GameManager header class

---
+++ include/core/Game.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "Board.hpp"
+#include "Player.hpp"
+#include "TurnManager.hpp"
+#include "Dice.hpp"
+#include "CardManager.hpp"
+#include "AuctionManager.hpp"
+#include "BankruptcyManager.hpp"
+#include "TradeManager.hpp"
+#include "LiquidationManager.hpp"
+#include "Logger.hpp"
+#include "Config.hpp"
+
+using namespace std;
+
+class Game {
+private:
+    Board board;
+    vector<Player> players;
+    TurnManager turnManager;
+    Dice dice;
+    CardManager cardManager;
+    AuctionManager auctionManager;
+    BankruptcyManager bankruptcyManager;
+    TradeManager tradeManager;
+    LiquidationManager liquidationManager;
+    Logger logger;
+    Config config;
+    bool gameOver;
+    
+public:
+    // Constructor
+    Game();
+    
+    // Destructor
+    ~Game() = default;
+
+    // Core Game Loop Methods
+    void run();
+    void startTurn();
+    void endTurn();
+    void handleLanding(Player& player); 
+    void checkWinCondition();
+    void executeCommand(const std::string& command);
+
+    // Getters / Setters    
+    Board& getBoard();
+    vector<Player>& getPlayers();
+    Player& getPlayer(int index);
+    TurnManager& getTurnManager();
+    Dice& getDice();
+    CardManager& getCardManager();
+    AuctionManager& getAuctionManager();
+    BankruptcyManager& getBankruptcyManager();
+    TradeManager& getTradeManager();
+    LiquidationManager& getLiquidationManager();
+    Logger& getLogger();
+    Config& getConfig();
+
+    bool isGameOver() const;
+    void setGameOver(bool status);
+};
---
+++ include/core/GameManager.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+class Game;
+
+class GameManager {
+private:
+    Game* currentGame;
+
+public:
+    // Constructor
+    GameManager();
+    
+    // Destructor
+    ~GameManager(); 
+
+
+    GameManager(const GameManager&) = delete;
+    GameManager& operator=(const GameManager&) = delete;
+
+    // Core Functionalities
+    void startNewGame();
+    void loadGame(const string& fileName);
+    void saveGame(const string& fileName) const;
+    
+    // Helper
+    void quitCurrentGame(); 
+
+    // Getters
+    Game* getCurrentGame() const;
+};
