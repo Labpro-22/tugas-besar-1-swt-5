af32c74e1b1fc10f95bd6230e553584acb165369
LutherCeltors <lutherceltors@gmail.com>
2026-04-26 16:54:24 +0700

    feat: implement saver for game state

--- src/data-layer/GameStateLoader.cpp
+++ src/data-layer/GameStateLoader.cpp
@@ -9,7 +9,165 @@
 //     }
 // }
 
+// int GameStateLoader::wordCounter(const string &line)
+// {
+//     int count = 0;
+//     bool inWord;
+//     for (char c : line)
+//     {
+//         if (isspace(c))
+//         {
+//             inWord = false;
+//         }
+//         else if (!inWord)
+//         {
+//             inWord = true;
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// string GameStateLoader::trimString(const string &line)
+// {
+//     const string whitespace = " \t\v\n\r\f";
+//     size_t start = line.find_first_not_of(whitespace);
+//     if (start == string::npos)
+//         return "";
+//     size_t end = line.find_last_not_of(whitespace);
+//     return line.substr(start, end - start + 1);
+// }
+
+// TurnManager GameStateLoader::loadTurn()
+// {
+//     string line, token, username;
+//     int currentPlayerIndex = -1;
+//     vector<string> usernamesVect;
+//     bool valid = false;
+//     int ctr, tChk, tNow, tMax;
+//     istringstream iss(line);
+//     if (getline(this->gameIfStream, line))
+//     {
+//         line = trimString(line);
+//         if (wordCounter(line) == 2)
+//         {
+//             if (iss >> token)
+//             {
+//                 ctr = 0;
+//                 tChk = stoi(token);
+//                 if (tChk == 0 && token.length() == 1)
+//                 {
+//                     tNow = tChk;
+//                     valid = true;
+//                 }
+//                 else
+//                 {
+//                     while (tChk > 0)
+//                     {
+//                         tChk /= 10;
+//                         ctr++;
+//                     }
+//                     if (ctr == token.length())
+//                     {
+//                         tNow = tChk;
+//                         valid = true;
+//                     }
+//                 }
+//                 if (valid)
+//                 {
+//                     valid = false;
+//                     if (iss >> token)
+//                     {
+//                         ctr = 0;
+//                         tChk = stoi(token);
+//                         if (tChk == 0 && token.length() == 1)
+//                         {
+//                             tMax = tChk;
+//                             valid = true;
+//                         }
+//                         else
+//                         {
+//                             while (tChk > 0)
+//                             {
+//                                 tMax /= 10;
+//                                 ctr++;
+//                             }
+//                             if (ctr == token.length())
+//                             {
+//                                 tMax = tChk;
+//                                 valid = true;
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         if (getline(this->gameIfStream, line))
+//         {
+//             if (wordCounter(line) == 1)
+//             {
+//                 istringstream iss(line);
+//                 if (iss >> token)
+//                 {
+//                     int jumlahPlayer = stoi(token);
+//                     int accountCtr = 0;
+//                     while (getline(this->gameIfStream, line) && accountCtr <= jumlahPlayer)
+//                     {
+//                         line = trimString(line);
+//                         if (wordCounter(line) == 4)
+//                         {
+//                             istringstream usEx(line);
+//                             if (usEx >> username)
+//                             {
+//                                 usernamesVect.push_back(username);
+//                             }
+//                             accountCtr++;
+//                         }
+//                     }
+//                     if (getline(this->gameIfStream, line))
+//                     {
+//                         line = trimString(line);
+//                         int nKartu = stoi(line);
+//                         for (int i = 0; i < nKartu; i++)
+//                             getline(this->gameIfStream, line);
+//                         if (getline(this->gameIfStream, line))
+//                         {
+//                             line = trimString(line);
+//                             if (wordCounter(line) == 1)
+//                             {
+//                                 for (size_t i = 0; i < usernamesVect.size(); i++)
+//                                 {
+//                                     if (usernamesVect[i].compare(line) == 0)
+//                                     {
+//                                         currentPlayerIndex = i;
+//                                         break;
+//                                     }
+//                                 }
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         TurnManager turnManager(vector<int>{0, 1, 2, 3, 4}, tMax);
+//         turnManager.setCurrentPlayerIndex(currentPlayerIndex);
+//         turnManager.setCurrentTurn(tNow);
+//     }
+// }
+
 // vector<Player> GameStateLoader::loadPlayers()
 // {
     
+// }
+
+// vector<PropertyTile> GameStateLoader::loadProperties()
+// {
+// }
+
+// CardManager GameStateLoader::loadDecks()
+// {
+// }
+
+// vector<LogEntry> loadLogs()
+// {
 // }
--- src/data-layer/GameStateSaver.cpp
+++ src/data-layer/GameStateSaver.cpp
@@ -1,23 +1,283 @@
-// #pragma once
-// #include <fstream>
-// #include <vector>
-// #include <map>
-// #include <tuple>
-// #include "../models/Player.hpp"
-// #include "../core/CardManager.hpp"
-// // #include "../models/LogEntry.hpp"
-// using namespace std;
+#include "GameStateSaver.hpp"
 
-// class GameStateSaver
-// {
-// private:
-//     ofstream gameOfStream;
+#include "Game.hpp"
+#include "Board.hpp"
+#include "Player.hpp"
+#include "AbilityCard.hpp"
+#include "DrawCardDeck.hpp"
+#include "CardManager.hpp"
+#include "Logger.hpp"
+#include "LogEntry.hpp"
+#include "PropertyTile.hpp"
+#include "Tile.hpp"
 
-// public:
-//     GameStateSaver(string path);
-//     ifstream getOfstream();
-//     void savePlayers(vector<Player> players);
-//     void saveProperties(vector<PropertyTile> properties);
-//     void saveDecks(CardManager newCardManager);
-//     // void saveLogs(vector<LogEntry>);
-// };
+#include <fstream>
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+std::string GameStateSaver::escapeValue(const std::string& value)
+{
+    std::string result;
+    result.reserve(value.size());
+
+    for (char c : value)
+    {
+        switch (c)
+        {
+            case '\\': result += "\\\\"; break;
+            case '\n': result += "\\n"; break;
+            case '\r': result += "\\r"; break;
+            case '\t': result += "\\t"; break;
+            case '=':  result += "\\="; break;
+            default:   result += c; break;
+        }
+    }
+
+    return result;
+}
+
+std::string GameStateSaver::boolToString(bool value)
+{
+    return value ? "true" : "false";
+}
+
+void GameStateSaver::writeRaw(std::ofstream& out, const std::string& text)
+{
+    out << text << '\n';
+}
+
+void GameStateSaver::writeString(std::ofstream& out, const std::string& key, const std::string& value)
+{
+    out << key << '=' << escapeValue(value) << '\n';
+}
+
+void GameStateSaver::writeInt(std::ofstream& out, const std::string& key, int value)
+{
+    out << key << '=' << value << '\n';
+}
+
+void GameStateSaver::writeSize(std::ofstream& out, const std::string& key, std::size_t value)
+{
+    out << key << '=' << value << '\n';
+}
+
+void GameStateSaver::writeBool(std::ofstream& out, const std::string& key, bool value)
+{
+    out << key << '=' << boolToString(value) << '\n';
+}
+
+std::string GameStateSaver::playerStatusToString(PlayerStatus status)
+{
+    switch (status)
+    {
+        case PlayerStatus::ACTIVE:   return "ACTIVE";
+        case PlayerStatus::JAILED:   return "JAILED";
+        case PlayerStatus::BANKRUPT: return "BANKRUPT";
+        default:                     return "UNKNOWN";
+    }
+}
+
+std::string GameStateSaver::propertyStatusToString(PropertyStatus status)
+{
+    switch (status)
+    {
+        case PropertyStatus::BANK:       return "BANK";
+        case PropertyStatus::OWNED:      return "OWNED";
+        case PropertyStatus::MORTGAGED:  return "MORTGAGED";
+        default:                         return "UNKNOWN";
+    }
+}
+
+std::string GameStateSaver::serializeAbilityCard(const AbilityCard* card)
+{
+    if (card == nullptr)
+    {
+        return "NULL";
+    }
+
+    return card->serialize();
+}
+
+template <typename T>
+std::string GameStateSaver::serializeDrawCard(const T* card)
+{
+    if (card == nullptr)
+    {
+        return "NULL";
+    }
+
+    return card->getDescription();
+}
+
+void GameStateSaver::savePlayers(std::ofstream& out, Game& game)
+{
+    std::vector<Player>& players = game.getPlayers();
+
+    writeRaw(out, "[PLAYERS]");
+    writeSize(out, "COUNT", players.size());
+
+    for (std::size_t i = 0; i < players.size(); ++i)
+    {
+        Player& player = players[i];
+
+        writeRaw(out, "BEGIN_PLAYER");
+        writeSize(out, "INDEX", i);
+        writeInt(out, "ID", player.getId());
+        writeString(out, "USERNAME", player.getUsername());
+        writeInt(out, "MONEY", player.getMoney());
+        writeInt(out, "POSITION", player.getPosition());
+        writeString(out, "STATUS", playerStatusToString(player.getStatus()));
+        writeInt(out, "JAIL_TURNS_ATTEMPTED", player.getJailTurnsAttempted());
+        writeBool(out, "USED_ABILITY_THIS_TURN", player.hasUsedAbilityThisTurn());
+        writeBool(out, "SHIELD_ACTIVE", player.isShieldActive());
+        writeInt(out, "DISCOUNT_PERCENT", player.getDiscountPercent());
+        writeInt(out, "DISCOUNT_DURATION", player.getDiscountDuration());
+        writeInt(out, "CONSECUTIVE_DOUBLE_COUNT", player.getConsecutiveDoubleCount());
+
+        const std::vector<std::unique_ptr<AbilityCard>>& handCards = player.getHandCards();
+        writeSize(out, "HAND_CARD_COUNT", handCards.size());
+
+        for (std::size_t j = 0; j < handCards.size(); ++j)
+        {
+            writeString(out, "HAND_CARD_" + std::to_string(j), serializeAbilityCard(handCards[j].get()));
+        }
+
+        writeRaw(out, "END_PLAYER");
+    }
+
+    writeRaw(out, "[/PLAYERS]");
+}
+
+void GameStateSaver::saveProperties(std::ofstream& out, Game& game)
+{
+    const std::vector<Tile*>& tiles = game.getBoard().getTiles();
+    std::vector<std::pair<std::size_t, PropertyTile*>> properties;
+
+    for (std::size_t i = 0; i < tiles.size(); ++i)
+    {
+        PropertyTile* property = dynamic_cast<PropertyTile*>(tiles[i]);
+        if (property != nullptr)
+        {
+            properties.push_back({i, property});
+        }
+    }
+
+    writeRaw(out, "[PROPERTIES]");
+    writeSize(out, "COUNT", properties.size());
+
+    for (std::size_t i = 0; i < properties.size(); ++i)
+    {
+        const std::size_t boardIndex = properties[i].first;
+        PropertyTile* property = properties[i].second;
+        Player* owner = property->getOwner();
+
+        writeRaw(out, "BEGIN_PROPERTY");
+        writeSize(out, "INDEX", i);
+        writeSize(out, "BOARD_INDEX", boardIndex);
+        writeString(out, "OWNER_USERNAME", owner == nullptr ? "NONE" : owner->getUsername());
+        writeInt(out, "OWNER_ID", owner == nullptr ? -1 : owner->getId());
+        writeString(out, "STATUS", propertyStatusToString(property->getStatus()));
+        writeInt(out, "MORTGAGE_VALUE", property->getMortgageValue());
+        writeInt(out, "FESTIVAL_MULTIPLIER", property->getFestivalMultiplier());
+        writeInt(out, "FESTIVAL_DURATION", property->getFestivalDuration());
+        writeRaw(out, "END_PROPERTY");
+    }
+
+    writeRaw(out, "[/PROPERTIES]");
+}
+
+template <typename T>
+void GameStateSaver::saveDrawCardDeck(std::ofstream& out, const std::string& sectionName, DrawCardDeck<T>& deck)
+{
+    const std::vector<T*>& pile = deck.getPile();
+
+    writeRaw(out, "[" + sectionName + "]");
+    writeSize(out, "COUNT", pile.size());
+
+    for (std::size_t i = 0; i < pile.size(); ++i)
+    {
+        writeString(out, "CARD_" + std::to_string(i), serializeDrawCard(pile[i]));
+    }
+
+    writeRaw(out, "[/" + sectionName + "]");
+}
+
+void GameStateSaver::saveAbilityCardDeck(std::ofstream& out, AbilityCardDeck& deck)
+{
+    writeRaw(out, "[ABILITY_DECK]");
+
+    const std::vector<std::unique_ptr<AbilityCard>>& drawPile = deck.getDrawPile();
+    writeSize(out, "DRAW_PILE_COUNT", drawPile.size());
+    for (std::size_t i = 0; i < drawPile.size(); ++i)
+    {
+        writeString(out, "DRAW_CARD_" + std::to_string(i), serializeAbilityCard(drawPile[i].get()));
+    }
+
+    const std::vector<std::unique_ptr<AbilityCard>>& discardedPile = deck.getDiscardedPile();
+    writeSize(out, "DISCARDED_PILE_COUNT", discardedPile.size());
+    for (std::size_t i = 0; i < discardedPile.size(); ++i)
+    {
+        writeString(out, "DISCARDED_CARD_" + std::to_string(i), serializeAbilityCard(discardedPile[i].get()));
+    }
+
+    writeRaw(out, "[/ABILITY_DECK]");
+}
+
+void GameStateSaver::saveCardManager(std::ofstream& out, Game& game)
+{
+    CardManager& cardManager = game.getCardManager();
+
+    writeRaw(out, "[CARD_MANAGER]");
+    saveDrawCardDeck(out, "CHANCE_DECK", cardManager.getChanceDeck());
+    saveDrawCardDeck(out, "COMMUNITY_DECK", cardManager.getCommunityDeck());
+    saveAbilityCardDeck(out, cardManager.getAbilityDeck());
+    writeRaw(out, "[/CARD_MANAGER]");
+}
+
+void GameStateSaver::saveLogs(std::ofstream& out, Game& game)
+{
+    std::vector<LogEntry> entries = game.getLogger().getEntries();
+
+    writeRaw(out, "[LOGS]");
+    writeSize(out, "COUNT", entries.size());
+
+    for (std::size_t i = 0; i < entries.size(); ++i)
+    {
+        const LogEntry& entry = entries[i];
+
+        writeRaw(out, "BEGIN_LOG");
+        writeSize(out, "INDEX", i);
+        writeInt(out, "TURN", entry.getTurn());
+        writeString(out, "USERNAME", entry.getUsername());
+        writeString(out, "ACTION_TYPE", entry.getActionType());
+        writeString(out, "DETAIL", entry.getDetail());
+        writeRaw(out, "END_LOG");
+    }
+
+    writeRaw(out, "[/LOGS]");
+}
+
+bool GameStateSaver::save(Game& game, const std::string& filePath)
+{
+    std::ofstream out(filePath);
+
+    if (!out.is_open())
+    {
+        return false;
+    }
+
+    writeRaw(out, "=== GAME STATE SAVE ===");
+    writeBool(out, "GAME_OVER", game.isGameOver());
+
+    savePlayers(out, game);
+    saveProperties(out, game);
+    saveCardManager(out, game);
+    saveLogs(out, game);
+
+    writeRaw(out, "=== END GAME STATE SAVE ===");
+
+    return out.good();
+}
