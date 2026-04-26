4d26576358007e163f5e60dd04d8a4b687094864
SlackingSlothh <zkyrbbn@gmail.com>
2026-04-26 22:31:50 +0700

    fix: reference loop for data change in Card effect

--- src/models/CommunityChestCard.cpp
+++ src/models/CommunityChestCard.cpp
@@ -7,7 +7,7 @@
 BirthdayCard::BirthdayCard() : CommunityChestCard(
     "Ini adalah hari ulang tahun Anda. Dapatkan M100 dari setiap pemain.",
     [](Player* p, Game* g) {
-        for (auto player : g->getPlayers()) {
+        for (Player& player : g->getPlayers()) {
             if (player.getId() != p->getId()) {
                 try {
                     try {
@@ -44,7 +44,7 @@ PayDoctorCard::PayDoctorCard() : CommunityChestCard(
 NyalegCard::NyalegCard() : CommunityChestCard(
     "Anda mau nyaleg. Bayar M200 kepada setiap pemain.",
     [](Player* p, Game* g) {
-        for (auto player : g->getPlayers()) {
+        for (Player& player : g->getPlayers()) {
             if (player.getId() != p->getId()) {
                 try {
                     try {
