2bf0b5c3f4aa19ab0cbfb6a7391f5e0bd869f9b6
SlackingSlothh <zkyrbbn@gmail.com>
2026-05-01 20:46:25 +0700

    feat: add load account on setup and save account on exit

--- src/views/Nimonspoli.cpp
+++ src/views/Nimonspoli.cpp
@@ -1,10 +1,13 @@
 #include "../../include/views/Nimonspoli.hpp"
+#include "../../include/data-layer/AccountDataManager.hpp"
 #include "raylib.h"
 
 void Nimonspoli::setup() {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT);
     InitWindow(1440, 980, "Nimonspoli");
     SetTargetFPS(60);
+    AccountDataManager accountLoader("./data/account.txt");
+    accountLoader.loadData(accountManager);
     sceneManager.initialize(&gameManager, &accountManager);
     sceneManager.setScene(SceneType::MainMenu);
 }
@@ -22,4 +25,7 @@ void Nimonspoli::start() {
 
 void Nimonspoli::update() { sceneManager.update(); }
 void Nimonspoli::draw()   { sceneManager.draw(); }
-Nimonspoli::~Nimonspoli() {}
+Nimonspoli::~Nimonspoli() {
+    AccountDataManager accountLoader("./data/account.txt");
+    accountLoader.saveData(accountManager);
+}
